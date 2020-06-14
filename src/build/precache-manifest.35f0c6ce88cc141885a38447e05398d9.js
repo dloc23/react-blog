ec95b35ad2949d21668f275624aa5593b72f6f40
dloc23 <derekhaase23@gmail.com>
2020-06-14 00:45:28 -0700

    first commit

---
+++ src/build/precache-manifest.35f0c6ce88cc141885a38447e05398d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e4025a29fe4742b11d",
+    "url": "/static/css/main.ace2b2dd.chunk.css"
+  },
+  {
+    "revision": "a3e4025a29fe4742b11d",
+    "url": "/static/js/main.7ac651f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5db49dec7cde47f601e",
+    "url": "/static/js/2.3457e373.chunk.js"
+  },
+  {
+    "revision": "1506cb1e290239a8dd7830036ddb487d",
+    "url": "/index.html"
+  }
+];
